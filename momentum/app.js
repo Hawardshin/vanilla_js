f8a2e55c10e25dc54b1b6d36733ae414a6c778ba
wnddms12345 <wnddms12345@naver.com>
2023-04-19 00:58:09 +0900

    study

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,30 @@
-alert("hi");
+// alert("hi");
+// const a = 5;
+// const b = 2;
+// let myName = "nico";
+// let c = true;
+// let d
+// let e = null;
+// console.log(a+b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log("hello " + myName);
+
+// myName = "nicolas";
+
+// console.log("your name is " + myName);
+
+// console.log(c);
+// console.log(d);
+// console.log(e);
+
+const player = {
+  name : "nico",
+  points : 10,
+  fat : true,
+};
+
+
+player.po = "jo";
+console.log(player);
+console.log(player.name);
