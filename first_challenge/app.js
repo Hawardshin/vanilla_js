f1ef4f086db465ef12be160e590c4a351fe86395
wnddms12345 <wnddms12345@naver.com>
2023-04-20 22:40:47 +0900

    for lecture 3.5 and challenge
    challenge done and Prac done

--- first_challenge/app.js
+++ first_challenge/app.js
@@ -17,3 +17,51 @@
 const title = document.querySelector(".hello h1");
 
 console.log(title);
+const title2 = document.querySelector(".hello:first-child h1");
+console.log(title2);
+const title3 = document.querySelector("div.hello:first-child h1");
+console.log(title3);
+console.dir(title3);
+
+
+
+function handleTitleClick() {
+  // console.log("title was clicked!");
+  title.style.color = "blue";
+}
+
+function handleMouseEnter(){
+  // console.log("mouse is here");
+  title.innerText = "mouse is here"
+}
+
+function handleWindowResize(){
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleMouseLeave(){
+  title.innerText = "mouse is gone!"
+}
+
+function handleWindowCopy(){
+  alert("copier!!");
+}
+
+function handleWindowOffline(){
+  alert("SoS no Wife");
+}
+
+function handleWindowOnline(){
+  alert("All GOOOOD");
+}
+
+
+// title.addEventListener("click",handleTitleClick);
+title.onclick = handleTitleClick;
+title.addEventListener("mouseenter",handleMouseEnter);
+title.addEventListener("mouseleave",handleMouseLeave);
+
+window.addEventListener("resize",handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
