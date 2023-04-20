67afb978e252d17ede147b811855f4d40a8bfdf4
Hawardshin <wnddms12345@naver.com>
2023-04-20 13:49:38 +0900

    code for lec 3-3

--- first_challenge/app.js
+++ first_challenge/app.js
@@ -1,8 +1,19 @@
-document.title = "hello title by js"
-const title = document.getElementById("title");
+// document.title = "hello title by js"
+// const title = document.getElementById("title");
 // console.log(title);
-console.dir(title);
-title.innerText = "Got you";
-console.log(title.id);
-console.log(title.className);
-//testtt
+// console.dir(title);
+// title.innerText = "Got you";
+// console.log(title.id);
+// console.log(title.className);
+//1st done
+
+
+// const hellos = document.getElementsByClassName("hello") //only for class
+// console.log(hellos);
+
+// const title = document.getElementsByTagName("h1"); //for all tag get
+// console.log(title);
+
+const title = document.querySelector(".hello h1");
+
+console.log(title);
