2bf0c4486ef0567c47ca32f4218f95b4419bba15
wnddms12345 <wnddms12345@naver.com>
2023-04-25 22:10:52 +0900

    lecture 4.6

--- lec4_login/app.js
+++ lec4_login/app.js
@@ -2,16 +2,31 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 const HIDDNE_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 function onLoginSubmit(event){
 	event.preventDefault();
 	// console.log(event);
 	loginForm.classList.add(HIDDNE_CLASSNAME);
-	const username = loginInput.value
+	const username = loginInput.value;
 	// greeting.innerText = "Hello " + username;
-	localStorage.setItem("username",username);
-	greeting.innerText = `Hello ${username}`;
-	greeting.classList.remove(HIDDNE_CLASSNAME);
+	localStorage.setItem(USERNAME_KEY,username);
+	paintGreetings(username);
 }
 
+function paintGreetings(username){
+  greeting.innerText = `Hello ${username}`
+  greeting.classList.remove(HIDDNE_CLASSNAME);
+}
 
 loginForm.addEventListener("submit",onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null){
+  //show the form
+  loginForm.classList.remove(HIDDNE_CLASSNAME);
+  loginForm.addEventListener("submit",onLoginSubmit);
+}else{
+  //show the greeting
+  paintGreetings(savedUsername);
+}
+
