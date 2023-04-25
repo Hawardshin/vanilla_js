774378b2d8b8b1ca89e820da7a670d55ca40b549
Hawardshin <wnddms12345@naver.com>
2023-04-25 13:31:09 +0900

    for lec 5

--- lec4_login/app.js
+++ lec4_login/app.js
@@ -1,9 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a")
+const greeting = document.querySelector("#greeting");
+const HIDDNE_CLASSNAME = "hidden";
 function onLoginSubmit(event){
-	tomato.preventDefault();
-	console.log(event);
+	event.preventDefault();
+	// console.log(event);
+	loginForm.classList.add(HIDDNE_CLASSNAME);
+	const username = loginInput.value
+	// greeting.innerText = "Hello " + username;
+	localStorage.setItem("username",username);
+	greeting.innerText = `Hello ${username}`;
+	greeting.classList.remove(HIDDNE_CLASSNAME);
 }
 
 
